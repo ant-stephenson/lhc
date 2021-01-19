274c0081f7841d749b5905febedac1b56e51bd15
Georgina Mansell <g.l.mansell@hotmail.co.uk>
2021-01-19 22:00:48 +0000

    Add more object documentation

--- R/objects.r
+++ R/objects.r
@@ -1,92 +1,112 @@
 #' Logistic model object class
 #'
-#' A reference class object to hold a logistic regression model.
-#' Can be used to initialize and train a model using the logistic_reg function,
-#' then use the model to predict the classes of new samples.
-#' Sample classes must be 0 or 1.
-#' Predict returns the probability that each new sample is class 1,
-#' and a decision threshold should be subsequently applied.
+#' This reference class object fits a binary classification model,
+#' using the logistic_reg function. The model can be used to
+#' predict the classes of new samples. The sample classes must be 0 or 1,
+#' and the prediction returns the estimated probabilities that each sample is class 1.
+#' A decision threshold should be subsequently.
 #'
 #' @name logistic_model
 #' @import methods
 #' @export logistic_model
 #' @exportClass logistic_model
 #'
-#' @field X An nxd matrix with samples as rows and features as columns
-#' @field y A length-n vector of 0s and 1s indicating true sample classes
-#' @field coeffs A length-d vector of model coefficients
+#' @field X An nxd matrix with samples as rows and features as columns.
+#' @field y A length-n vector of 0s and 1s indicating true sample classes.
+#' @field coeffs A length-d vector of model coefficients.
 logistic_model <- setRefClass("logistic_model",
-                              fields = c(
-                                X = "matrix",
-                                y = "numeric",
-                                coeffs = "numeric"
-                              ),
-                              methods = list(
-                                initialize = function(X, y){
-                                  "Provide X and y and the coeffs field will be calculated using logistic_reg"
-                                  .self$X <- as.matrix(X)
-                                  .self$y <- as.numeric(y)
-                                  .self$coeffs <- as.numeric(logistic_reg(as.matrix(X), as.numeric(y)))
-                                },
-                                predict = function(X_test){
-                                  "Provide a matrix of new samples and a vector of P(y=1) is returned"
-                                  prob <- logisticf(as.matrix(X_test) %*% coeffs)
-                                  return(as.numeric(prob))
-                                }
-                              )
+  fields = c(
+    X = "matrix",
+    y = "numeric",
+    coeffs = "numeric"
+  ),
+  methods = list(
+    initialize = function(X, y){
+      "Provide X and y and the coeffs field will be calculated using logistic_reg"
+      .self$X <- as.matrix(X)
+      .self$y <- as.numeric(y)
+      .self$coeffs <- as.numeric(logistic_reg(as.matrix(X), as.numeric(y)))
+    },
+    predict = function(X_test){
+      "Provide a matrix of new samples and a vector of P(y=1) is returned"
+      prob <- logisticf(as.matrix(X_test) %*% coeffs)
+      return(as.numeric(prob))
+    }
+  )
 )
 
-ROC_data <- setRefClass("ROC_data",
-                        fields = c(
-                          thresholds = "numeric",
-                          FP = "numeric",
-                          TP = "numeric",
-                          auc = "numeric"))
-ROC_data$methods(
-  initialize = function(y, prob){
-    #we want to find a set of thresholds that vary FP and FN rate from 0 to 1
-    #find n evenly spaced values from the list of probabilities, including the 0 and 1
-    prob_ordered <- prob[order(prob)]
-    n <- 30
-    indices <- 1 + 0:(n-3) * (length(y)-1)/(n-3)
-    thresh <- c(0, prob_ordered[round(indices)], 1)
+#' ROC curve object class
+#'
+#' This reference class object is used to plot a Receiver Operating Characteristic curve.
+#' An ROC curve is a performance measure of a classification model,
+#' created by plotting the true positive rate (TPR) against the false positive rate (FPR)
+#' as the decision threshold is varied. The object finds suitable thresholds,
+#' calculates FPR and TPR at each, and can calculate the Area Under the Curve (AUC).
+#' A vector of true sample classifications (0 or 1) and a vector of estimated probabilities
+#' from a model are needed to initialise.
+#'
+#' @name ROC_curve
+#' @import methods
+#' @export ROC_curve
+#' @exportClass ROC_curve
+#'
+#' @field thresholds A vector of 30 decision thresholds.
+#' @field FP A vector of the false positive rate at each threshold.
+#' @field TP A vector of the true positive rate at each threshold.
+#' @field auc A numeric that is the area under the ROC curve.
+ROC_curve <- setRefClass("ROC_curve",
+  fields = c(
+    thresholds = "numeric",
+    FP = "numeric",
+    TP = "numeric",
+    auc = "numeric"
+  ),
+  methods = list(
+    initialize = function(y, prob){
+      "Provide sample labels and probabilites, and the FPR and TPR are calculated at 30 decision thresholds"
+      #find n thresholds that vary FP and FN rate from 0 to 1
+      prob_ordered <- prob[order(prob)]
+      n <- 30
+      indices <- 1 + 0:(n-3) * (length(y)-1)/(n-3)
+      thresh <- c(0, prob_ordered[round(indices)], 1)
 
-    #for each threshold, calculate the FR, FN, s and b
-    FPr <- rep(NA, n)
-    TPr <- rep(NA, n)
-    for(i in 1:n){
-      FPr[i] <- sum(prob > thresh[i] & y == 0) / sum(y == 0)
-      TPr[i] <- sum(prob > thresh[i] & y == 1) / sum(y == 1)
-    }
-
-    .self$FP <- FPr
-    .self$TP <- TPr
-    .self$auc <- 0
-  },
-
-  calc_auc = function(){
-    if(.self$auc == F){
-      #using the trapezoidal rule to integrate the roc curve to find the auc
-      #since we only have points not a curve I think this is approach is sufficent
-      #note that the points go from right to left on the curve (so FP[i] > FP[i+1])
-      AUC <- 0
-      for(i in 1:(length(FP)-1)){
-        h <- FP[i] - FP[i+1]
-        AUC <- AUC + (h/2 * (TP[i] + TP[i+1]))
+      #for each threshold, calculate the FR, FN, s and b
+      FPr <- rep(NA, n)
+      TPr <- rep(NA, n)
+      for(i in 1:n){
+        FPr[i] <- sum(prob > thresh[i] & y == 0) / sum(y == 0)
+        TPr[i] <- sum(prob > thresh[i] & y == 1) / sum(y == 1)
       }
-      .self$auc <- AUC
-      return(AUC)
-    }
-  },
 
-  #define a method to plot the roc
-  plot_curve = function(){
-    .self$calc_auc()
-    plot(0:1, 0:1, type="l", lty=2, xlab="False Positive Rate",
-         ylab="True Positive Rate", main=paste("AUC:", round(.self$auc,2)))
-    lines(.self$FP, .self$TP, col="steelblue")
-    legend("bottomright", legend=c("Model", "Chance"), col=c("steelblue", 1), lty=1:2)
-  }
+      #add results to the object fields
+      .self$thresholds <- thresh
+      .self$FP <- FPr
+      .self$TP <- TPr
+      .self$auc <- 0
+    },
+    calc_auc = function(){
+      "If the AUC has not already be calculated, this calls the calculation."
+      if(.self$auc == F){
+        #use the trapezoidal rule to integrate the ROC curve to find the AUC
+        #since we have n points not a curve this is sufficient
+        AUC <- 0
+        for(i in 1:(length(FP)-1)){
+          h <- FP[i] - FP[i+1]
+          AUC <- AUC + (h/2 * (TP[i] + TP[i+1]))
+        }
+        .self$auc <- AUC
+        return(AUC)
+      }
+    },
+    plot_curve = function(){
+      "Plot the ROC curve."
+      .self$calc_auc()
+      plot(0:1, 0:1, type="l", lty=2, xlab="False Positive Rate",
+           ylab="True Positive Rate", main=paste("AUC:", round(.self$auc,2)))
+      lines(.self$FP, .self$TP, col="steelblue")
+      legend("bottomright", legend=c("Model", "Chance"), col=c("steelblue", 1), lty=1:2)
+    }
+  )
 )
 
 
